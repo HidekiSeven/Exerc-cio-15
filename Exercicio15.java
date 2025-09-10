454b6e3ff8b2b8dda36d113e50ab2895a5ed2b95
HidekiSeven <gustavokanomato@gmail.com>
2025-09-10 19:36:20 -0300

    Update and rename README.md to Exercicio15.java
    Crie um programa Java que leia o número de dias trabalhados em um mês e mostre o
    salário de um funcionário, sabendo que ele trabalha 8 horas por dia e ganha R$25 por
    hora trabalhada.

---
+++ Exercicio15.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SalarioMensal {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+
+        double horasPorDia = 8;
+        double valorPorHora = 25.00;
+
+
+        System.out.print("Digite o número de dias trabalhados no mês: ");
+        int diasTrabalhados = scanner.nextInt();
+
+        double salarioTotal = diasTrabalhados * horasPorDia * valorPorHora;
+
+
+        System.out.printf("O salário do funcionário por " + diasTrabalhados + " dias trabalhados é de R$%.2f\n", salarioTotal);
+
+        scanner.close();
+    }
+}
